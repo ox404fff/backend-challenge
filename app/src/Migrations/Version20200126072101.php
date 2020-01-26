ec5eb19b1ef42825730e440bb32ce700264efc2f
ox404fff <ox404fff@gmail.com>
2020-01-26 08:27:36 +0100

    feature-api-endpoint: Create index

---
+++ app/src/Migrations/Version20200126072101.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200126072101 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql(
+<<<SQL
+ALTER TABLE user_course_sessions ADD INDEX user_course_sessions_user_id_status_idx(user_id, status)
+SQL
+        );
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql(
+<<<SQL
+ALTER TABLE user_course_sessions DROP INDEX (user_course_sessions_user_id_status_idx);
+SQL
+        );
+    }
+}
