3c1a0c1c8f6378ffea944e66963e85b677ec6fa8
ox404fff <ox404fff@gmail.com>
2020-01-26 08:30:22 +0100

    feature-api-endpoint: Clean generated code

--- app/src/Repository/UserCourseRepository.php
+++ app/src/Repository/UserCourseRepository.php
@@ -18,33 +18,4 @@ class UserCourseRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, UserCourses::class);
     }
-
-    // /**
-    //  * @return UserCourses[] Returns an array of UserCourses objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?UserCourses
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
